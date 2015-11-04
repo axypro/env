d063bfa3d8aaf1f03f10d32d170225181e551df0
Oleg Grigoriev <go.vasac@gmail.com>
2015-11-04 18:05:55 +0300

    Env: wrapping of global functions

--- Env.php
+++ Env.php
@@ -8,9 +8,23 @@ namespace axy\env;
 
 use axy\env\helpers\Normalizer;
 use axy\errors\InvalidConfig;
+use axy\errors\FieldNotExist;
+use axy\errors\Disabled;
 
 /**
  * Wrapper for the environment
+ *
+ * @method void header(string $string, bool $replace = true, int $http_response_code = null)
+ * @method bool setcookie(string $name,string $val,int $e=0,string $p=null,string $d=null,bool $s=false,bool $h = false)
+ * @method array getallheaders()
+ * @method array headers_list()
+ * @method int http_response_code(int $response_code = null)
+ * @method string ini_set(string $varname, string $newvalue)
+ * @method string ini_get(string $varname)
+ * @method string getenv(string $varname)
+ * @method int error_reporting(int $level = null)
+ * @method string date_default_timezone_get()
+ * @method bool date_default_timezone_set(string $timezone_identifier)
  */
 class Env
 {
@@ -39,16 +53,54 @@ class Env
     /**
      * Checks if a function exists (global or overloaded)
      *
-     * @param string $functionName
+     * @param string $name
      * @return bool
      */
-    public function isFunctionExists($functionName)
+    public function isFunctionExists($name)
     {
         $functions = $this->config->functions;
-        if (array_key_exists($functionName, $functions)) {
-            return ($functions[$functionName] !== null);
+        if (array_key_exists($name, $functions)) {
+            return ($functions[$name] !== null);
         }
-        return function_exists($functionName);
+        return function_exists($name);
+    }
+
+    /**
+     * Calls a function
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     * @throws \axy\errors\InvalidConfig
+     *         the function in the config is not callable
+     * @throws \axy\errors\Disabled
+     *         the function is disabled in the config
+     * @throws \axy\errors\FieldNotExist
+     *         the function is not found
+     * @throws \Exception
+     *         an exception inside the function
+     */
+    public function __call($name, $arguments)
+    {
+        $functions = $this->config->functions;
+        if (array_key_exists($name, $functions)) {
+            if ($functions[$name] === null) {
+                throw new Disabled($name, null, $this);
+            }
+            $callback = $functions[$name];
+            if (!is_callable($callback)) {
+                if (!is_callable($callback, true)) {
+                    $message = 'Function "'.$name.'" is not callable';
+                    throw new InvalidConfig('Env', $message, 0, null, $this);
+                }
+                throw new FieldNotExist($name, 'global', null, $this);
+            }
+            return call_user_func_array($callback, $arguments);
+        }
+        if (!function_exists($name)) {
+            throw new FieldNotExist($name, 'global', null, $this);
+        }
+        return call_user_func_array($name, $arguments);
     }
 
     /**
--- tests/EnvTest.php
+++ tests/EnvTest.php
@@ -57,15 +57,120 @@ class EnvTest extends \PHPUnit_Framework_TestCase
         $config = [
             'functions' => [
                 'time' => 'myTime',
-                'my_function_that_not_exists_in_global' => 'myFunc',
+                'my_function_that_not_exist_in_global' => 'myFunc',
                 'strlen' => null,
             ],
         ];
         $env = new Env($config);
         $this->assertTrue($env->isFunctionExists('time'));
-        $this->assertTrue($env->isFunctionExists('my_function_that_not_exists_in_global'));
+        $this->assertTrue($env->isFunctionExists('my_function_that_not_exist_in_global'));
         $this->assertTrue($env->isFunctionExists('strtotime'));
         $this->assertFalse($env->isFunctionExists('strlen'));
-        $this->assertFalse($env->isFunctionExists('function_9231m123n_does__not_exists'));
+        $this->assertFalse($env->isFunctionExists('function_9231m123n_does__not_exist'));
+    }
+
+    /**
+     * @dataProvider providerCall
+     * @param string $name
+     * @param array $arguments
+     * @param mixed $result
+     * @param string $exception [optional]
+     */
+    public function testCall($name, $arguments, $result, $exception = null)
+    {
+        $functions = [
+            'strlen' => function ($string) {
+                return strlen($string) * 3;
+            },
+            'my_defined_function' => function () {
+                return array_sum(func_get_args());
+            },
+            'date' => null,
+            'invalid' => true,
+            'exc' => function ($x) {
+                if ($x > 1) {
+                    throw new \InvalidArgumentException($x.' > 1');
+                }
+            }
+        ];
+        $config = [
+            'functions' => $functions,
+        ];
+        $env = new Env($config);
+        if (!$exception) {
+            $this->assertSame($result, $env->__call($name, $arguments));
+        } else {
+            $this->setExpectedException($exception);
+            $env->__call($name, $arguments);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function providerCall()
+    {
+        return [
+            'global' => [
+                'str_replace',
+                ['a', 'b', 'tra-la-la'],
+                'trb-lb-lb',
+            ],
+            'override' => [
+                'strlen',
+                ['string'],
+                18,
+            ],
+            'my' => [
+                'my_defined_function',
+                [1, 2, 3],
+                6,
+            ],
+            'disable' => [
+                'date',
+                ['Y-m-d H:i:s'],
+                null,
+                'axy\errors\Disabled',
+            ],
+            'unknown' => [
+                'this__is_U_nkn_own_functions',
+                [1, 2, 3],
+                null,
+                'axy\errors\FieldNotExist',
+            ],
+            'invalid' => [
+                'invalid',
+                [1, 2, 3],
+                null,
+                'axy\errors\InvalidConfig',
+            ],
+            'exception' => [
+                'exc',
+                [5],
+                null,
+                'InvalidArgumentException',
+            ],
+        ];
+    }
+
+    public function testCallMagic()
+    {
+        $headers = [];
+        $config = [
+            'functions' => [
+                'header' => function ($h) use (&$headers) {
+                    $headers[] = $h;
+                }
+            ],
+        ];
+        $env = new Env($config);
+        $this->assertSame(3, $env->strlen('123'));
+        $env->header('Content-Type: text/html; charset=utf-8');
+        $env->header('X-Header: 5');
+        $expected = [
+            'Content-Type: text/html; charset=utf-8',
+            'X-Header: 5',
+        ];
+        $this->assertSame($expected, $headers);
     }
 }
